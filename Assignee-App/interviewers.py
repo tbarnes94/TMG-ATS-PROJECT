94f24b6ca1c90857c24ecdf2ebba4ab04a75eb6b
tbarnes94 <thomas.jonathan.barnes@gmail.com>
2016-07-12 15:36:48 -0500

    merging

--- Assignee-App/interviewers.py
+++ Assignee-App/interviewers.py
@@ -1,4 +1,5 @@
-import json, random
+import json, random, datetime
+CURRENT_DATE = datetime.datetime.now().replace(hour=0,minute=0,second=0,microsecond=0)
 def get_interviewer(my_list):
 	num_interviewers = len(my_list)
 	interviewer_index = random.randint(0,num_interviewers-1)
@@ -6,13 +7,14 @@ def get_interviewer(my_list):
 
 
 def main():
-	with open('interviewers.json', 'rb') as fh:
+	global CURRENT_DATE
+	with open('interviewers-%s.json'%CURRENT_DATE, 'rb') as fh:
 		my_dict = json.load(fh)
 
 	# print json.dumps(my_dict, indent=4, sort_keys=True, separators=(",", ": "))
-	print 'Round 1: ' + get_interviewer(my_dict[0]['Intern'][0]['Trader']['Round1'])
-	print 'Round 2: ' + get_interviewer(my_dict[0]['Intern'][0]['Trader']['Round2'])
-	print 'Round 3: ' + get_interviewer(my_dict[0]['Intern'][0]['Trader']['Round3'])
+	print 'Round 1: ' + get_interviewer(my_dict[0]["Locations"]["Chicago"]["Degrees"]["Undergraduate"]["FULL TIME"]["Requisitions"]["RV Trader"]["Round 1"])
+	print 'Round 2: ' + get_interviewer(my_dict[0]["Locations"]["Chicago"]["Degrees"]["Undergraduate"]["FULL TIME"]["Requisitions"]["RV Trader"]["Round 2"])
+	print 'Round 3: ' + get_interviewer(my_dict[0]["Locations"]["Chicago"]["Degrees"]["Undergraduate"]["FULL TIME"]["Requisitions"]["RV Trader"]["Round 3"])
 
 if __name__ == "__main__":
 	main()
